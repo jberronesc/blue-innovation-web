7d52938319b5791e559a3d05c0478a22044248a8
jefferson <berronesjefferson@gmail.com>
2025-01-03 22:42:57 -0500

    Update: dialog confirm

--- app/(client)/shared/ui/view-modals/ViewModelConfirmModal.tsx
+++ app/(client)/shared/ui/view-modals/ViewModelConfirmModal.tsx
@@ -24,6 +24,7 @@ export const ViewModelConfirmModal = ({
 
   const onSuccessWrapper = () => {
     onSuccess();
+    setShow(false);
   };
   const onCancelWrapper = () => {
     if (onCancel) onCancel();
